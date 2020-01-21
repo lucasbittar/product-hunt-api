f38ec264d3eb1bee862c226ac5313a9bd2fcce69
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2020-01-21 12:25:37 -0300

    Product Hunt API complete

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Init app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect DB
+const databaseUri = 'mongodb://localhost:27017/nodeapi';
+const connect = async () => {
+  console.log(`Connecting to DB - uri: ${databaseUri}`);
+  return mongoose.connect(databaseUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+connect()
+  .then(() => {
+    console.log('Database connected!');
+  })
+  .catch((e) => {
+    console.log('Something went wrong: ', e.message);
+  });
+
+requireDir('./src/models');
+
+// Routes
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
+
+// docker pull mongo
+// docker run --name mongodb -p 27017:27017 -d mongo
+// docker ps
